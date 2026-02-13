23b6fb75309bbc5c881b18c004550d1281ee3ec9
Acervo Fana <fabio.vianas.sa@gmail.com>
2026-02-13 00:58:57 -0300

    conexão Flask + MongoDB Atlas

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+import os
+from flask import Flask, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+# Pega a string de conexão do ambiente do Vercel
+mongo_uri = os.environ.get("MONGODB_URI")
+client = MongoClient(mongo_uri)
+
+# Substitua "meubanco" pelo nome do banco que você criou no Atlas
+db = client["meubanco"]
+
+# Substitua "filmes" pelo nome da coleção
+collection = db["filmes"]
+
 @app.route("/")
 def home():
-    return "Meu acervo de filmes está online!"
+    # Busca todos os documentos da coleção, sem mostrar o _id
+    filmes = list(collection.find({}, {"_id": 0}))
+    return jsonify(filmes)
